7582436d144dae1b55d245fbbe677670d20df19e
jufrin01 <jufrinaha111@gmail.com>
2024-11-28 14:29:20 +0700

    add . learn micro service eureka.. ##1

---
+++ Confiq-Server/src/test/java/com/example/confiqserver/ConfiqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confiqserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiqServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
